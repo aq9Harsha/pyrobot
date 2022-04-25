06726dd90f83968c5577b9de6517bc3ec8e36943
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2022-04-25 17:47:29 -0400

    Install torchvision in cuda

--- docker/install_scripts/install_pytorch_cuda.sh
+++ docker/install_scripts/install_pytorch_cuda.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 rm -rf /var/lib/apt/lists/*
 
 # install pytotorch
-pip install torch --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113
